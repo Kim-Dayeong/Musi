5456cdefc1f376cfa63cfd1aab01ed221d999062
Kim-Dayeong <kkimdayeongg@gmail.com>
2023-01-18 21:22:52 +0900

    create project

---
+++ src/main/java/com/musi/musishop/MusiShopApplication.java
@@ -0,0 +1,13 @@
+package com.musi.musishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musi/musishop/MusiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musi.musishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
